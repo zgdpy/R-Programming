ad9551da5a281e1076d536c9e7d4d50b4e3665cd
Mike <mike190519@gmail.com>
2022-10-24 11:15:07 +0800

    adding content

--- R Programming.R
+++ R Programming.R
@@ -1,1 +1,29 @@
 # This is for the coursera course of R Programming
+attributes()
+NaN
+Inf
+1/Inf
+vector()
+matrix()
+dim()
+list()
+vector('list', length = 2)
+lm()
+glm()
+factor()
+table()
+unclass()
+attr()
+read.table()
+read.csv()
+x <- factor(c("yes", "yes", "no", "yes", "no"), levels = c("yes", "no"))
+
+row.names() # for data.frame
+nrow()
+ncol()
+colnames() # for matrix 
+rownames() # for matrix
+as.matrix()
+data.matrix()
+names() # for data.frame column names
+dimnames()
