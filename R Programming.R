f6173d1b22a68c12d380259acac8903888cb6e23
Mike <mike190519@gmail.com>
2022-10-24 11:17:02 +0800

    adding content

--- R Programming.R
+++ R Programming.R
@@ -3,6 +3,8 @@ attributes()
 NaN
 Inf
 1/Inf
+# NA values have a class also, so there are integer NA, character NA, etc.
+# A NaN value is also NA but the converse is not true
 vector()
 matrix()
 dim()
@@ -16,6 +18,7 @@ unclass()
 attr()
 read.table()
 read.csv()
+# The order of the levels of a factor can be set using the levels argument to factor().
 x <- factor(c("yes", "yes", "no", "yes", "no"), levels = c("yes", "no"))
 
 row.names() # for data.frame
@@ -27,3 +30,4 @@ as.matrix()
 data.matrix()
 names() # for data.frame column names
 dimnames()
+
